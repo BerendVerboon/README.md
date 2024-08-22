28eedf5c89ab13ad51302a3ea083f84a449bd38c
Berend <berend.cunt@fuckface.com>
2024-08-22 13:14:08 +0200

    .

--- sxhkd/lock.sh
+++ sxhkd/lock.sh
@@ -1,21 +1,18 @@
 #!/bin/bash
 
 # Set the path to your video file
-VIDEO_PATH="video.mp4"
+VIDEO_PATH="/home/john/.config/sxhkd/video.mp4"
 
-# Get the screen resolution
-RESOLUTION=$(xrandr | grep '*' | awk '{print $1}')
+# Save environment values that may get changed
+export XSECURELOCK_SAVER=saver_mpv
+export XSECURELOCK_DISCARD_FIRST_KEYPRESS=0
+export XSECURELOCK_SHOW_DATETIME=0
+export XSECURELOCK_SHOW_USERNAME=0
+export XSECURELOCK_PASSWORD_PROMPT=time_hex
+export XSECURELOCK_FONT=-misc-fixed-bold-r-normal--18-120-100-100-c-90-iso8859-15
 
-# Loop the video until the screen is locked
-mpv --no-audio --loop=inf --fullscreen --ontop --no-stop-screensaver --no-input-default-bindings --input-conf=/dev/null --title=lockscreen "$VIDEO_PATH" &
+# Define the mpv saver
+export SAVER_MPV_COMMAND="mpv --really-quiet --fullscreen --no-stop-screensaver --no-input-default-bindings --input-conf=/dev/null --loop=inf --no-audio \"$VIDEO_PATH\""
 
-# Wait for the video player to open
-sleep 1
-
-# Now lock the screen. i3lock doesn't provide a proper way to put an overlay on top of other windows, 
-# so the video will be underneath the blank i3lock screen.
-# This is a limitation and xsecurelock could be considered for a more rich approach that can display videos on top.
-i3lock
-
-# After unlocking, kill mpv
-pkill -f "mpv --no-audio --loop=inf --fullscreen --ontop --no-stop-screensaver --no-input-default-bindings --input-conf=/dev/null --title=lockscreen"
+# Start xsecurelock
+xsecurelock
