cc11348222b9ad1b8488df2c9a562047ce159260
Berend <berend.cunt@fuckface.com>
2024-08-22 13:29:35 +0200

    .

--- sxhkd/lock.sh
+++ sxhkd/lock.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 
-# Set the path to your video file
-VIDEO_PATH="/home/john/.config/sxhkd/video.mp4"
-
 # Save environment values that may get changed
-export XSECURELOCK_SAVER=saver_mpv
+export XSECURELOCK_SAVER="mpv --really-quiet --fullscreen --no-stop-screensaver --no-input-default-bindings --input-conf=/dev/null --loop=inf --no-audio /home/john/.config/sxhkd/video.mp4"
 export XSECURELOCK_DISCARD_FIRST_KEYPRESS=0
 export XSECURELOCK_SHOW_DATETIME=0
 export XSECURELOCK_SHOW_USERNAME=0
 export XSECURELOCK_PASSWORD_PROMPT=time_hex
 export XSECURELOCK_FONT=-misc-fixed-bold-r-normal--18-120-100-100-c-90-iso8859-15
 
-# Define the mpv saver
-export SAVER_MPV_COMMAND="mpv --really-quiet --fullscreen --no-stop-screensaver --no-input-default-bindings --input-conf=/dev/null --loop=inf --no-audio \"$VIDEO_PATH\""
-
 # Start xsecurelock
 xsecurelock
+
+
+
+
+
