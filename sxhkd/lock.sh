dd6fe3bc3f9150027d13906f48857901fc1999ce
Berend <berend.cunt@fuckface.com>
2024-08-22 14:23:31 +0200

    .

--- sxhkd/lock.sh
+++ sxhkd/lock.sh
@@ -1,12 +1,3 @@
 #!/bin/bash
 
-# Save environment values that may get changed
-export XSECURELOCK_SAVER="/home/john/.config/sxhkd/screen_saver.sh"
-export XSECURELOCK_DISCARD_FIRST_KEYPRESS=0
-export XSECURELOCK_SHOW_DATETIME=0
-export XSECURELOCK_SHOW_USERNAME=0
-export XSECURELOCK_PASSWORD_PROMPT=time_hex
-export XSECURELOCK_FONT=-misc-fixed-bold-r-normal--18-120-100-100-c-90-iso8859-15
-
-# Start xsecurelock
-xsecurelock
+i3lock -i /home/john/.config/sxhkd/lockscreen.jpeg
--- sxhkd/screen_saver.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-mpv --really-quiet --fullscreen --no-stop-screensaver --no-input-default-bindings --input-conf=/dev/null --loop=inf --no-audio /home/john/.config/sxhkd/video.mp4
